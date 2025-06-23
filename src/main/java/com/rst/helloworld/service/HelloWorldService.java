ef4a6bd4c2f217cdb6b3d7dcd789265a176b7c15
SHAIK KHAJA PASHA <shaiksulthan20698@gmail.com>
2025-06-23 22:21:24 +0530

    mycode

---
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -0,0 +1,32 @@
+package com.rst.helloworld.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class HelloWorldService {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldService.class);
+
+	public String getDesc() {
+
+		logger.debug("getDesc() is executed!");
+
+		return "Git, GitHub, Maven, CICD, Jenkins, Docker, Kubernetes, Terraform, ArgoCD and Amazon Web Services";
+
+	}
+
+	public String getTitle(String name) {
+
+		logger.debug("getTitle() is executed! $name : {}", name);
+
+		if(StringUtils.isEmpty(name)){
+			return "Hello from Mithun Technologies";
+		}else{
+			return "Hello " + name;
+		}
+		
+	}
+}
